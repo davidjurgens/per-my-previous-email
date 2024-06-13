bcb446c52106e758b8e5e29ad3ae9bff2f8c5fc7
Kenan Alkiek <kalkiek@Kenans-MacBook-Air.local>
2024-06-13 14:19:18 -0400

    Make predictions on a single file/GPU combo

--- src/models/make_predictions.py
+++ src/models/make_predictions.py
@@ -1,63 +1,47 @@
 import argparse
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(description="Script to run predictions with a specified GPU and data file.")
-    parser.add_argument('--gpu', required=True, help="GPU number to use")
-    parser.add_argument('--data_path', required=True, help="Path to the input data file")
-    return parser.parse_args()
-
-
-args = parse_args()
-
-# Set CUDA GPU before importing torch and transformers
+import logging
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-
-# Now it's safe to import heavy libraries
 import pandas as pd
 import torch
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-MODEL_PATH = '/shared/4/projects/research-jam-2024/models/fine-tuned/roberta-base/checkpoint-864'
-TOKENIZER = 'roberta-base'
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+LABELS = ['Sharing', 'Requesting', 'Promising', 'Personal', 'Pleasantries', 'Spam', 'ResponseExpected']
 
 
-def main(data_fp):
-    out_dir = '/shared/4/projects/research-jam-2024/working-dir/model-predictions/'
-
-    data_file_name = os.path.basename(data_fp).split('/')[-1]  # Name of the month e.g. en.2009-11.tsv
-    output_csv_fp = os.path.join(out_dir, f'{data_file_name}')
-
-    df = pd.read_csv(data_fp, sep='\t')[['message_id', 'message_body_clean']]
-
-    tokenizer = AutoTokenizer.from_pretrained(TOKENIZER)
-    model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH).to('cuda')
-    model.eval()
-
-    labels = [
-        'label_Act:::Thank you/Welcome', 'label_Act:::Commit/Agree', 'label_Act:::Request',
-        'label_Act:::Deliver/Informative', 'label_Act:::Other_merged',
-        'label_spam', 'label_Sender Expectation'
-    ]
-
-    predictions_list = []
-
-    for index, text in enumerate(tqdm(df['message_body_clean'], total=len(df))):
-        pred_dict = run_predictions(tokenizer, model, [text], labels)[0]
-        predictions_list.append(pred_dict)
-
-        if (index + 1) % 25000 == 0:
-            save_intermediate_results(predictions_list, df, index, output_csv_fp)
-
-    save_final_results(predictions_list, df, output_csv_fp)
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Run model predictions on text data.")
+    parser.add_argument('--model_path', type=str,
+                        default="/shared/4/projects/research-jam-2024/models/fine-tuned/roberta-base/checkpoint-181")
+    parser.add_argument('--data_fp', type=str, required=True, help='File path for the data to be processed.')
+    parser.add_argument('--out_dir', type=str,
+                        default="/shared/4/projects/research-jam-2024/working-dir/model-predictions/")
+    parser.add_argument('--batch_size', type=int, default=256, help='Batch size for processing the data.')
+    return parser.parse_args()
 
 
-def run_predictions(tokenizer, model, texts, labels):
-    inputs = tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
+def load_data(file_path):
+    logging.info(f"Loading data from {file_path}")
+    df = pd.read_csv(file_path, sep='\t')
+    df = df[['message_id', 'message_body_clean']].rename(columns={'message_id': 'id', 'message_body_clean': 'text'})
+    return df
+
+
+def setup_model(model_path):
+    logging.info("Setting up the model...")
+    model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=7,
+                                                               problem_type="multi_label_classification")
+    tokenizer = AutoTokenizer.from_pretrained('roberta-base')
+    model = model.to('cuda')
+    return model, tokenizer
+
+
+def run_predictions(model, tokenizer, texts, labels):
+    logging.info(f"Running predictions on {len(texts)} texts")
+    inputs = tokenizer(texts, padding=True, truncation=True, max_length=350, return_tensors="pt")
     inputs = {k: v.to('cuda') for k, v in inputs.items()}
 
     with torch.no_grad():
@@ -65,27 +49,42 @@ def run_predictions(tokenizer, model, texts, labels):
     predictions = torch.sigmoid(outputs.logits)
     predictions = predictions.cpu().numpy()
 
-    predictions_dicts = []
-    for pred in predictions:
-        pred_dict = {label: float(pred_val) for label, pred_val in zip(labels, pred)}
-        predictions_dicts.append(pred_dict)
-
+    predictions_dicts = [{label: float(pred_val) for label, pred_val in zip(labels, pred)} for pred in predictions]
     return predictions_dicts
 
 
-def save_intermediate_results(predictions_list, df, index, output_csv_fp):
-    temp_df = pd.DataFrame(predictions_list)
-    temp_final_df = pd.concat([df.iloc[:index + 1]['message_id'], temp_df], axis=1)
-    temp_final_df.to_csv(output_csv_fp, sep='\t', index=False)
-    print(f"Saved {index + 1} rows to {output_csv_fp}")
+def main():
+    args = parse_arguments()
+    df = load_data(args.data_fp)
+    model, tokenizer = setup_model(args.model_path)
+    predictions_list = []
+
+    model.eval()
+
+    logging.info(f"Starting prediction batches of size {args.batch_size}")
+
+    file_name = os.path.basename(args.data_fp)
+    output_csv_fp = os.path.join(args.out_dir, file_name)
+
+    for i in tqdm(range(0, len(df), args.batch_size), desc="Processing text batches"):
+        batch_texts = df['text'][i:i + args.batch_size].tolist()
+        batch_predictions = run_predictions(model, tokenizer, batch_texts, LABELS)
+        predictions_list.extend(batch_predictions)
+
+        # Save complete results every 100 batches
+        if (i // args.batch_size) % 100 == 99:
+            complete_df = pd.DataFrame(predictions_list)
+            complete_df = pd.concat([df['id'].iloc[:len(complete_df)], complete_df], axis=1)
+            complete_csv_fp = output_csv_fp.replace('.csv', f'_up_to_batch_{i // args.batch_size + 1}.csv')
+            complete_df.to_csv(complete_csv_fp, index=False)
+            logging.info(f"Saved complete results up to batch {i // args.batch_size + 1} to {complete_csv_fp}")
+
+    # Save the final results
+    final_df = pd.DataFrame(predictions_list)
+    final_df = pd.concat([df['id'], final_df], axis=1)
+    final_df.to_csv(output_csv_fp, index=False)
+    logging.info(f"Final output saved to {output_csv_fp}")
 
 
-def save_final_results(predictions_list, df, output_csv_fp):
-    predictions_df = pd.DataFrame(predictions_list)
-    final_df = pd.concat([df['message_id'], predictions_df], axis=1)
-    final_df.to_csv(output_csv_fp, sep='\t', index=False)
-    print(f"Final predictions saved to {output_csv_fp}")
-
-
-if __name__ == '__main__':
-    main(args.data_path)
+if __name__ == "__main__":
+    main()
